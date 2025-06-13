6d6e2f0364a6a3d441a40592266b19554fac4d57
jm-brabant <jm.brabant@oceavia.nc>
2025-06-13 12:02:14 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 def classFactory(iface):
-    from .batch_csv_wkt_importer import BatchCSVWKTImporter
+    from .batch_csv_wkt_importer_Fr import BatchCSVWKTImporter
     return BatchCSVWKTImporter(iface)
